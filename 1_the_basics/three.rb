ad20bb9fc859edbc9d204812a4f244ee10771195
Charlotte Goldthwaite <chargold201@gmail.com>
2023-03-20 16:45:38 -0400

    Add basics exercises two and three

---
+++ 1_the_basics/three.rb
@@ -0,0 +1,7 @@
+movies = {
+  "The Big Lebowski": 1998,
+  "2001: A Space Odyssey": 1968,
+  "Empire Records": 1995
+}
+
+movies.each { |key, value| puts value }
---
+++ 1_the_basics/two.rb
@@ -0,0 +1,12 @@
+num = 4573
+
+thousands = num / 1000
+hundreds = num % 1000 / 100
+tens = num % 100 / 10
+ones = num % 10
+
+puts num
+puts thousands
+puts hundreds
+puts tens
+puts ones
